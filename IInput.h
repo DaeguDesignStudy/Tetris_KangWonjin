0ecb7172b7b1bad80c5257e86ebc5d92d74f53e6
wonjin92 <kangwonjin92@gmail.com>
2020-01-13 23:16:01 +0900

    Input module을 조금 더 구체화 함

--- IInput.h
+++ IInput.h
@@ -1,7 +1,39 @@
 #pragma once
+#include <queue>
+#include <atomic>
+#include <thread>
+
+#include "Command.h"
 
 class IInput
 {
+    std::thread m_inputThread;
+
+protected:
+    std::atomic<bool> m_running = false;
+    std::queue<Command> m_inputQueue; // thread-safe queue로 변경해야 함
+
 public:
-    virtual Command GetInput() = 0;
+    Command GetInput()
+    {
+        auto command = m_inputQueue.front();
+        m_inputQueue.pop();
+        return command;
+    }
+
+    void StartListenInput()
+    {
+        m_running.store(true);
+        m_inputThread = std::thread(&IInput::runningloop, this);
+    }
+
+    void Stop()
+    {
+        m_running.store(false);
+        m_inputThread.join();
+    }
+
+private:
+    // 자식 클래스에서 m_inputQueue에 데이터를 입력하는 부분을 작성해야함
+    virtual void runningloop() = 0;
 };
--- KeyboardInput.h
+++ KeyboardInput.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <iostream>
+
+#include <WinUser.h>
+
 #include "IInput.h"
 #include "Command.h"
 
 class KeyboardInput : public IInput
 {
-public:
-    Command GetInput() override
+    void runningloop() override
     {
+        while (m_running.load())
+        {
+            SHORT b = GetAsyncKeyState(VK_LEFT);
+
+            std::cout << "LEFT = " << b << "\n";
+        }
     }
 };
